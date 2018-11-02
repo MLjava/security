a3aad40a96f424f0d926febeea5897bfde6520cf
linma <linma@homeinns.com>
2018-11-02 18:17:13 +0800

    security

--- security-server/src/main/java/com/utils/PasswordEncoder.java
+++ security-server/src/main/java/com/utils/PasswordEncoder.java
@@ -15,7 +15,6 @@ public class PasswordEncoder {
 
     public static void main(String[] args) {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-//        String s = new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + UUID.randomUUID().toString();
         System.out.println(bCryptPasswordEncoder.encode("admin"));
     }
 }
