91129a97983666dc6b2f38bf16c90181b1cb6991
linma <linma@homeinns.com>
2018-11-21 11:23:52 +0800

    添加了refresh token

--- security-server/src/main/java/com/SecurityApplication.java
+++ security-server/src/main/java/com/SecurityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
--- security-server/src/main/java/com/config/Oauth2Config.java
+++ security-server/src/main/java/com/config/Oauth2Config.java
@@ -1,9 +1,11 @@
 package com.config;
 
+import com.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -35,6 +38,9 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private TokenStore tokenStore;
 
+    @Autowired
+    private UserService userService;
+
     /**
      * 告诉Spring Security Token的生成方式
      *
@@ -50,6 +56,7 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
         endpoints.tokenStore(tokenStore())
                 .tokenEnhancer(tokenEnhancerChain)
                 .authenticationManager(authenticationManager);
+//                .userDetailsService(userService);
     }
 
     @Override
@@ -57,7 +64,8 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
         oauthServer
                 //允许所有资源服务器访问公钥端点（/oauth/token_key）
                 //只允许验证用户访问令牌解析端点（/oauth/check_token）
-                .tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
                 // 允许客户端发送表单来进行权限认证来获取令牌
                 .allowFormAuthenticationForClients();
     }
@@ -91,7 +99,7 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
                         .createDelegatingPasswordEncoder()
                         .encode("secret"))
                 // 该client允许的授权类型
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes("authorization_code", "password", "refresh_token")
                 // 允许的授权范围
                 .scopes("app")
                 //登录后绕过批准询问(/oauth/confirm_access)
@@ -107,6 +115,16 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
     @Bean
     public TokenEnhancer tokenEnhancer() {
         return new CustomTokenEnhancer();
+    }
+
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
     }
 
 }
